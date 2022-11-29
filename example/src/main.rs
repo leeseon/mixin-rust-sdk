128e285010fce65a9dfbf1ce9de949f58593815a
Sean Lee <leeseon@gmail.com>
2022-11-29 17:45:53 +0800

    first step add the workspace layout

---
+++ example/src/main.rs
@@ -0,0 +1,6 @@
+use mixin_sdk::Client;
+
+fn main() {
+    let client = Client::new();
+    println!("{:?}", client);
+}
---
+++ mixin-sdk/src/lib.rs
@@ -0,0 +1,10 @@
+#[derive(Debug)]
+pub struct Client {
+
+}
+
+impl Client {
+    pub fn new() -> Client {
+        Client {}
+    }
+}
